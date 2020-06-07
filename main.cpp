a56b026678c4b8640a7b1c20a2d6cbecbb15cec2
Joss Whittle <joss.whittle@gmail.com>
2020-06-07 18:39:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
 
     constexpr int   TRAIN_GAMES   = 50000;
     constexpr int   TRAIN_EPOCHS  = 50;
-    constexpr float LEARNING_RATE = (1e-2) / 1.5;
+    constexpr float LEARNING_RATE = (1e-2); // / 1.5;
 
     std::ofstream log_train = Agent::log_training_csv("./logs/log_train.csv");
     std::ofstream log_eval  = Agent::log_evaluation_csv("./logs/log_eval.csv");
@@ -18,23 +18,25 @@ int main() {
     Agent::PhaseParams params;
 
     // Start from the last phase and work backwards towards 2048
-    for (int phase = Agent::NUM_PHASES, epoch = 0; phase >= 0; phase--) {
+    for (int phase = 0, epoch = 0; phase <= Agent::NUM_PHASES; phase++) {
+
+        const int end_phase = std::min((phase + 2), Agent::END_PHASE);
 
         for (int phase_epoch = 0; phase_epoch < TRAIN_EPOCHS; phase_epoch++, epoch++) {
 
             std::cout << std::endl << "Phase " << phase << " Phase Epoch " << phase_epoch << " Epoch " << epoch << std::endl;
 
             // Perform one epoch of training
-            Agent::train_agent(epoch, TRAIN_GAMES, phase, Agent::END_PHASE, LEARNING_RATE, params, log_train);
+            Agent::train_agent(epoch, TRAIN_GAMES, phase, end_phase, LEARNING_RATE, params, log_train);
 
             // Save the current params for all phases greater than and including the current phase
-            for (int i = phase; i < Agent::NUM_PHASES; i++) {
-                Agent::save(STRING( "./logs/params phase_" << phase << "_" << i << ".bin" ), params[i]);
-            }
+//            for (int i = 0; i <= phase; i++) {
+//                Agent::save(STRING( "./logs/params phase_" << phase << "_" << i << ".bin" ), params[i]);
+//            }
 
             // Play evaluation games and log stats
-            Agent::evaluate_agent(epoch, 1000, phase, Agent::END_PHASE, 1, params, log_eval);
-            Agent::evaluate_agent(epoch, 100,  phase, Agent::END_PHASE, 2, params, log_eval);
+            Agent::evaluate_agent(epoch, 1000, 0, end_phase, 1, params, log_eval);
+            Agent::evaluate_agent(epoch, 100,  0, end_phase, 2, params, log_eval);
         }
     }
 
